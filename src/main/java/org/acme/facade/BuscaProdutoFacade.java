819cf75fbae17fab503656c9ca02b03958722871
Lucas Gobbi <lucasmegobbi@gmail.com>
2025-08-20 03:27:53 -0300

    fix: retorna mensagem de erro apropriada quando nao encontra produto

--- src/main/java/org/acme/facade/BuscaProdutoFacade.java
+++ src/main/java/org/acme/facade/BuscaProdutoFacade.java
@@ -2,6 +2,7 @@ package org.acme.facade;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.acme.dao.ProdutoDao;
 import org.acme.dto.SimulacaoRequest;
@@ -23,7 +24,10 @@ public class BuscaProdutoFacade {
             log.info("Nenhum produto encontrado para as condições desejadas: Valor = {}, Prazo = {}", request.getValorDesejado(), request.getPrazo());
         }
 
-        return produtoOpt.get();
+        return produtoOpt.orElseThrow(() -> {
+            log.warn("Nenhum produto encontrado para as condições desejadas: Valor = {}, Prazo = {}", request.getValorDesejado(), request.getPrazo());
+            return new NotFoundException("Nenhum produto de crédito disponível para os critérios informados.");
+        });
     }
 
 }
